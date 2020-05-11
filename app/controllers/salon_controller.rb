d9b6fbc9c995028038cbca053a8a6c3dadc9867a
poteto033 <nhnhnnhn033@gmail.com>
2020-05-11 10:52:53 +0900

    店舗詳細情報作成
    ・トップページから店舗詳細ページに繊維できるようにした。
    ・店舗詳細ページに店舗情報を表示できるようにした。

--- app/controllers/salon_controller.rb
+++ app/controllers/salon_controller.rb
@@ -2,4 +2,8 @@ class SalonController < ApplicationController
   def index
     @salons=Salon.all
   end
+
+  def show
+    @salon=Salon.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20200510222333) do
 
   create_table "salons", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
-    t.integer "postcode", null: false
+    t.string "postcode", null: false
     t.string "prefecture", null: false
     t.string "city", null: false
     t.string "block", null: false
