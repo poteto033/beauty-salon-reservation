8b49da1fcb67fa9db70c9dd77582c4d07b2d6315
poteto033 <nhnhnnhn033@gmail.com>
2020-05-11 12:31:45 +0900

    予約ページへの遷移
    ・店詳細ページから予約ページへ遷移できるようにした。その際に、ログインしていない場合、ログイン画面に遷移するようにした。

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -1,2 +1,12 @@
 class ReservationController < ApplicationController
+  def new
+    if user_signed_in?     
+    else
+      redirect_to new_user_session_path
+      
+    end
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root "salon#index"
-  resources :salon, only: [:show]
+  resources :salon, only: [:show] do
+    member do
+      resources :reservation, only: [:new,:create]
+    end
+  end
 
 end
