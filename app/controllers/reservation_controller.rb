8b72bafb8e72289488a6a6100ea22b40612ce4d3
poteto033 <nhnhnnhn033@gmail.com>
2020-05-13 22:07:45 +0900

    予約キャンセル機能実装
    ・一度した予約をキャンセルできるようにした。

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -19,6 +19,12 @@ class ReservationController < ApplicationController
     end
   end
 
+  def destroy
+    @reservation=Reservation.find_by(user_id: current_user.id)
+    @reservation.destroy
+    redirect_to root_path
+  end
+
   private
   def reservation_params
       params.require(:reservation).permit(:name,:gender,:cut,:pama,:color,:month,:hour,:day,:minute).merge(user_id: current_user.id,salon_id: params[:salon_id])
