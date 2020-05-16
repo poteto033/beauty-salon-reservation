5ab836b2b6f52b69025f02082471562c5d167d11
poteto033 <nhnhnnhn033@gmail.com>
2020-05-16 17:53:52 +0900

    ユーザー関連のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :resevarion
+  validates :nickname,:email,:first_name,:last_name,
+  :first_name_kana,:last_name_kana,:gender,:birthday,:phone_number,presence: true
+
+  validates :nickname,:email,uniqueness: true
+
+  validates :last_name,:first_name,format: { with: /\A^[ぁ-んァ-ン一-龥]\z/,message: 'を全角で入力してください'}
+  validates :last_name_kana,:first_name_kana, format: { with: /\A^([ァ-ン]|ー)+$\z/,message: 'を全角カナで入力してください'}
+  validates :phone_number,format: {with: /A^\d{10}$|^\d{11}$Z/, message: 'をハイフンなしで10桁or11桁で入力してください'}
+
 end
