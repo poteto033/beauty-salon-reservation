35d8ad5a088efc5b0e76a27b6c235952c13bbfe6
poteto033 <nhnhnnhn033@gmail.com>
2020-05-17 11:53:59 +0900

    ユーザーテーブル修正
    ・ユーザーテーブルカラムに電話番号を追加し、バリデーションと登録フォームを追加した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
 
   validates :nickname,:email,uniqueness: true
 
-  validates :last_name,:first_name,format: { with: /\A^[ぁ-んァ-ン一-龥]\z/,message: 'を全角で入力してください'}
+  validates :last_name,:first_name,format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/,message: 'を全角で入力してください'}
   validates :last_name_kana,:first_name_kana, format: { with: /\A^([ァ-ン]|ー)+$\z/,message: 'を全角カナで入力してください'}
-  validates :phone_number,format: {with: /A^\d{10}$|^\d{11}$Z/, message: 'をハイフンなしで10桁or11桁で入力してください'}
+  validates :phone_number,format: {with:  /\A\d{10}$|^\d{11}\z/, message: 'をハイフンなしで10桁or11桁で入力してください'}
 
 end
